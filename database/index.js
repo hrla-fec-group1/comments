7fc18c3d2596fb492c9523f76f59d004226a760d
Steven Turnbull <steven.turnbull901@gmail.com>
2018-11-10 10:40:23 -0800

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/comments';
+const mongoUri = 'mongodb://mongo:27017/comments';
 
 const db = mongoose.connect(mongoUri);
 
